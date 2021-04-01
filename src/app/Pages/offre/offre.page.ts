ad9e8ec8807ab4c6cdcb80e7fffbc00685a2d60d
Zotoavina <zotoavinanantenaina@gmail.com>
2021-04-01 20:15:45 +0200

    de

--- src/app/Pages/offre/offre.page.ts
+++ src/app/Pages/offre/offre.page.ts
@@ -1,3 +1,4 @@
+import { OffreService } from './../../services/offre.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offre.page.scss'],
 })
 export class OffrePage implements OnInit {
-
-  constructor() { }
+  offres : any;
+  message : any;
+  constructor(private offre : OffreService) { }
 
   ngOnInit() {
   }
+
+  listOffres(){
+    const success = data => {
+        console.log(data);
+        if(data.status == 200){
+          this.offres = data.data;
+        }
+        this.message = data.message;
+    }
+
+    const error = data => {
+        console.log(data);
+        this.message = data.error.message;
+    }
+    this.offre.listOffres().subscribe(success, error);
+  }
+
+  
+
+
 
 }
