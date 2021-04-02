514e63dc147ec76f60ca2f7972c7cca69e5d4a7e
Njaratiana <mnjaratiana@gmail.com>
2021-04-02 22:23:49 +0200

    b

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,11 +19,6 @@ export class AppComponent implements OnInit {
       icon: 'book'  
     },
     {
-      title: 'Info Consommation',
-      url: '/data',
-      icon: 'information-circle'  
-    },
-    {
       title: 'Achat De Forfait',
       url: '/offre',
       icon: 'albums'
